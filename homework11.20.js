d731aec0a282c38a6cc00b4f0ced2c604808a696
yurimlee <ylee981352@gmail.com>
2021-11-20 09:01:29 +0900

    [#3] items firebase 연동 create 복습

---
+++ homework11.20.js
@@ -0,0 +1,6 @@
+//++ 추가하기
+const cartsCheckboxObject = document.getElementsByName('carts-checkbox')[index];
+// 체크 박스에 접근한다.
+      cartsCheckboxObject.key = key;
+      // 인풋태그 갔다가 결국 키값을 넘긴다.
+    
