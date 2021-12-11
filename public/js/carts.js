1f0e5a9fb74406cf9ce4aa856be033254eb72e70
yurimlee <ylee981352@gmail.com>
2021-12-11 18:38:27 +0900

    [#5] 정렬 아이콘 클릭시 이동 후 해당 아이콘 활성화

--- public/js/carts.js
+++ public/js/carts.js
@@ -1,7 +1,27 @@
-
 let carts;
 
-// const queryString = new URLSearchParams(window.location.search);
+const queryString = new URLSearchParams(window.location.search);
+const orderByName = queryString.get('orderByName') || '';
+const orderByType = queryString.get('orderByType') || '';
+
+// if(orderByName === 'name'&& orderByType === 'asc'){
+//   document.getElementsByClassName('orderBy')[0].classList.add('active');
+// }  else if (orderByName === 'name'&& orderByType === 'aesc'){
+//   document.getElementsByClassName('orderBy')[1].classList.add('active');
+// }
+// if(orderByName === 'enter'&& orderByType === 'asc'){
+//   document.getElementsByClassName('orderBy')[2].classList.add('active');
+// }  else if (orderByName === 'enter'&& orderByType === 'aesc'){
+//   document.getElementsByClassName('orderBy')[3].classList.add('active');
+// }
+// if(orderByName === 'expire'&& orderByType === 'asc'){
+//   document.getElementsByClassName('orderBy')[4].classList.add('active');
+// }  else if (orderByName === 'expire'&& orderByType === 'aesc'){
+//   document.getElementsByClassName('orderBy')[5].classList.add('active');
+// }
+
+document.getElementById(orderByName + '-' + orderByType).classList.add('active');
+
 // const nameText = queryString.get('input-text');
 // // const inputTextObjects = document.getElementsByName('input-text');
 // // const inputTextObject = inputTextObjects[0];
@@ -9,7 +29,6 @@ let carts;
 // inputTextObject.value = nameText;
 // inputTextObject.focus();
 // inputTextObject.blur();
-
 
 
 const cartsCreate = function(form) {
@@ -24,7 +43,6 @@ const cartsCreate = function(form) {
     cartsRead();
   });
 };
-
 
 const cartsRead = function() {
    const promises = [];
@@ -45,7 +63,6 @@ const cartsRead = function() {
       const tagDivParent = document.getElementById('tag-tbody-parent');
     tagDivParent.innerHTML = '';
     const tagDivChild = document.getElementById('tag-tr-child');
-    
     for (let key in carts) {
       carts[key].k = key
     }
@@ -74,14 +91,10 @@ const cartsRead = function() {
     });
 };
 
-
-
 const cartsDelete = function(key) {
   const url = 'https://red-javascript-yurim-default-rtdb.firebaseio.com/carts/'+ key + '.json';
   axios.delete(url).then(cartsRead);
 };
-
-
 
 const cartsUpdate = function(index, key) {
   const url = 'https://red-javascript-yurim-default-rtdb.firebaseio.com/carts/' + key + '.json';
@@ -102,7 +115,6 @@ const itemsChange = function(event) {
     console.log(event)
     console.log(url);
     axios.patch(url, carts[event.target.key]);
-    axios.post('https://red-javascript-yurim-default-rtdb.firebaseio.com/items.json', carts[event.target.key]);
   } else {
     console.log('TODO: item 삭제')
     axios.delete(url);
