a5a9f3176a7e691ff822b2471730a255c5181b71
yurimlee <ylee981352@gmail.com>
2022-01-15 21:06:41 +0900

    [#4] items 순서 적용

--- public/js/items.js
+++ public/js/items.js
@@ -41,8 +41,8 @@ const itemsRead = function() {
       if(_items[i].name.indexOf(q)< 0) continue;
       const newDivChild = tagTrChild.cloneNode(true);
       tagTbodyParent.appendChild(newDivChild);
-      // const itemsIndexObject = document.getElementsByName('items-index')[index];
-      // itemsIndexObject.innerHTML = index + 1;
+      const itemsIndexObject = document.getElementsByName('items-index')[index];
+      itemsIndexObject.innerHTML = index + 1;
       const itemsNameObject = document.getElementsByName('items-name')[index];
       itemsNameObject.innerHTML = _items[i].name;
       const itemsEnterObject = document.getElementsByName('items-enter')[index];
